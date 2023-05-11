003ee0b021eb3258f9d5ff33612a8246e0588d44
fariz.wisnu <fariz.wisnu@bluebirdgroup.com>
2023-05-11 09:48:41 +0700

    Number of Steps to Reduce a Number to Zero

--- numberOfStage/main.go
+++ numberOfStage/main.go
@@ -1,22 +1,46 @@
+//package main
+//
+//import "fmt"
+//
+//func numOfStage(angka int) int {
+//	langkah := 0
+//
+//	for angka > 0 {
+//		if angka%2 == 0 {
+//			angka /= 2
+//		} else {
+//			angka -= 1
+//		}
+//		langkah++
+//	}
+//	return langkah
+//
+//}
+//
+//func main() {
+//	angka := 90
+//	fmt.Printf("dari angka %v, ini memeiliki %v langkah pengurangan untuk sampai menjadi 0\n",// angka, numOfStage(angka))
+//}
+
 package main
 
 import "fmt"
 
-func numOfStage(angka int) int {
-	langkah := 0
-
-	for angka > 0 {
-		if angka%2 == 0 {
-			angka /= 2
+func findStepNum(num int) int {
+	step := 0
+	for num > 0 {
+		if num%2 == 0 {
+			num /= 2
 		} else {
-			angka -= 1
+			num -= 1
 		}
-		langkah++
+
+		step++
 	}
-	return langkah
+	return step
 }
 
 func main() {
-	angka := 90
-	fmt.Printf("dari angka %v, ini memeiliki %v langkah pengurangan untuk sampai menjadi 0\n", angka, numOfStage(angka))
+	angka := 48
+	fmt.Printf("Dari angka ini %v memiliki jumlah langkah %vx untuk agar menjadi 0 ", angka, findStepNum(angka))
 }
