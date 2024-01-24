6fdec1ad92e76b8b09f942fda5ae0e2a29962a7f
fariz.wisnu <fariz.wisnu@bluebirdgroup.com>
2024-01-24 10:15:18 +0700

    Add containsDuplicate function to check for duplicates in an array

---
+++ contains duplicate/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func containsDuplicate(nums []int) bool {
+	// make seen for store value in slice of array bool
+	seen := make(map[int]bool)
+	
+	// for loop for range of nums
+	for _, num := range nums {
+		fmt.Println("seen", seen)
+		if seen[num] {
+			return true
+		}
+		seen[num] = true
+	}
+	return false
+}
+
+func main() {
+	nums := []int{1,2,3}
+	fmt.Println(containsDuplicate(nums))
+}
